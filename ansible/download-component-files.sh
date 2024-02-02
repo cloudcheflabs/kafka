4ac7b7c39a7173fb30ba9cfd3fc910c6c73659b3
Cloud Chef Labs <admin@cloudchef-labs.com>
2024-02-02 10:01:21 +0900

    init.

---
+++ ansible/download-component-files.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# download jdk.
+curl -L -O https://github.com/cloudcheflabs/chango-libs/releases/download/chango-private-deps/openlogic-openjdk-17.0.7+7-linux-x64.tar.gz
+mv openlogic-openjdk-17.0.7+7-linux-x64.tar.gz roles/jdk/files/
+
+# download maven.
+curl -L -O https://github.com/cloudcheflabs/chango-libs/releases/download/chango-private-deps/apache-maven-3.9.3-bin.tar.gz
+mv apache-maven-3.9.3-bin.tar.gz roles/maven/files/
+
+# download zookeeper.
+curl -L -O https://github.com/cloudcheflabs/chango-libs/releases/download/chango-private-deps/apache-zookeeper-3.6.4-bin.tar.gz
+mv apache-zookeeper-3.6.4-bin.tar.gz roles/zookeeper/files/
+
+# download kafka
+curl -L -O https://github.com/cloudcheflabs/chango-libs/releases/download/chango-private-deps/confluent-community-7.4.0.tar.gz
+mv confluent-community-7.4.0.tar.gz roles/kafka/files/
+
+
+# download bastion for ansible.
+curl -L -O https://github.com/cloudcheflabs/chango-libs/releases/download/chango-private-deps/bastion-venv.tar.gz
+
+
+
